2618dacb28c733666d90d5381056e9bb62961a03
MikiWojak <mikizarowa8@gmail.com>
2020-04-26 20:12:29 +0200

    Inicjacja
    - Dodano klase Main i metode main()
    - Wypisano "Hello world" na poczatek

---
+++ src/Main/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Main;
+
+/**
+ * @author MikiWojak
+ * Klasa, która uruchamia g³ówne okno programu
+ */
+public class Main {
+	
+	/**
+	 * G³ówna metoda. S³u¿y do uruchomienia programu
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+}
