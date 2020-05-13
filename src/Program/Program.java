71b7489061d68a1fabab5f8f93fb6d3f059038d3
MikiWojak <mikizarowa8@gmail.com>
2020-05-13 20:38:49 +0200

    Więcej niż jeden panel - przygotowanie
    - Dodano pola do klasy Program zw. z Panelami
    - Dodano osobna funkcje do inicjowania komponentow

--- src/Program/Program.java
+++ src/Program/Program.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
@@ -15,16 +16,39 @@ import javax.swing.border.EmptyBorder;
 public class Program extends JFrame {
 	
 	/**
+	 * Pojemnik na kilka paneli.
+	 * Panele te bêd¹ w³¹czane w zaleznoœci od potrzeb.
+	 */
+	private JLayeredPane warstwy;
+	
+	/**
+	 * Panel widoku grup i s³ówek.
+	 */
+	private Panel widok;
+	
+	/**
 	 * Utworzenie g³ównego programu.
 	 * Konstruktor klasy Program.
 	 */
 	public Program() {
+		inicjujKomponenty();
+	}
+	
+	/**
+	 * Inicjowanie komponentów w programie.
+	 */
+	private void inicjujKomponenty() {
+		//podstawowe parametry
 		setBounds(100, 100, 0, 0);
-		add(new Panel());
-		pack();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setResizable(false);
 		setTitle("S³ówka 2");
+		
+		//komponenty
+		add(new Panel());
+		pack();
+		
+		//koñcowe parametry
 		setVisible(true);
 	}
 
