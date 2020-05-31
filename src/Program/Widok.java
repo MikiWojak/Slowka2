032f34e21522eb5bd2d489685a973ddf73836d8e
MikiWojak <mikizarowa8@gmail.com>
2020-05-31 20:45:20 +0200

    Widok interfejs 1
    - Dodano JList - lista grup
    - Dodano JTable - tabela slowek

--- src/Program/Widok.java
+++ src/Program/Widok.java
@@ -8,6 +8,8 @@ import org.slf4j.LoggerFactory;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.SwingConstants;
+import javax.swing.JList;
+import javax.swing.JTable;
 
 /**
  * Panel z widokiem na grupy i s³owa.
@@ -15,6 +17,7 @@ import javax.swing.SwingConstants;
  */
 public class Widok extends Panel {
 	private JLabel lblWidok;
+	private JTable table;
 	
 	/**
 	 * Utworzenie panelu z widokiem.
@@ -35,8 +38,16 @@ public class Widok extends Panel {
 		lblWidok = new JLabel("Widok");
 		lblWidok.setHorizontalAlignment(SwingConstants.CENTER);
 		lblWidok.setFont(new Font("Arial", Font.BOLD, 16));
-		lblWidok.setBounds(0, 13, 1200, 20);
+		lblWidok.setBounds(400, 0, 800, 50);
 		add(lblWidok);
+		
+		JList list = new JList();
+		list.setBounds(0, 0, 350, 550);
+		add(list);
+		
+		table = new JTable();
+		table.setBounds(400, 50, 800, 500);
+		add(table);
 	}
 	
 	/**
