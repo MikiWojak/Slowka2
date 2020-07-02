775a9b465008bb940f7aa542889b64035b6a0e03
MikiWojak <mikizarowa8@gmail.com>
2020-07-02 18:52:33 +0200

    Okienko Dodaj/Modyfikuj slowo - przygotowanie
    - Dodano niezbedne elementy do okeinka
    - Nadano nazwy elementom
    - Ustawiono elementy jako pola
    - Zapisano komentarze do dokumentacji

--- src/Edycja/PanelSlowo.java
+++ src/Edycja/PanelSlowo.java
@@ -5,6 +5,8 @@ import java.awt.Dimension;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 /**
  * Panel do edycji s³ów.
@@ -24,15 +26,43 @@ public class PanelSlowo extends Panel {
 	 * Napis informuj¹cy czy nowe s³owo, czy edycja istniej¹cego.
 	 */
 	private JLabel lblOpis;
+	/**
+	 * Pole na s³owo w jêzyku obcym.
+	 */
+	private JTextField tfSlowo;
+	/**
+	 * Pole na t³umaczenie s³owa.
+	 */
+	private JTextField tfTlumaczenie;
+	/**
+	 * Pole na czêœæ mowy s³owa.
+	 */
+	private JTextField tfCzescMowy;
+	/**
+	 * Opis pola na s³owo w jêzyku obcym.
+	 */
+	private JLabel lblSlowo;
+	/**
+	 * Opis pola na t³umaczenie s³owa.
+	 */
+	private JLabel lblTlumaczenie;
+	/**
+	 * Opis pola na czêœæ mowy s³owa.
+	 */
+	private JLabel lblCzescMowy;
+	/**
+	 * Przycisk do dodawania lub modyfikacji s³owa.
+	 */
+	private JButton btnPotwierdzAkcje;
 	
 	/**
 	 * Konstruktor klasy Slowo - nowe s³owo.
 	 * @param id_grupa ID grupy, do której ma nale¿eæ s³owo
+	 * @wbp.parser.constructor
 	 */
 	public PanelSlowo(int id_grupa) {
 		this.id_grupa = id_grupa;
 		inicjujKomponenty();
-		lblOpis.setText("Dodaj s³owo do grupy o ID: " + this.id_grupa);
 	}
 	
 	/**
@@ -50,19 +80,60 @@ public class PanelSlowo extends Panel {
 	 * Inicjuje komponenty.
 	 */
 	private void inicjujKomponenty() {
-		setPreferredSize(new Dimension(800, 600));
+		setPreferredSize(new Dimension(800, 225));
 		
-		lblOpis = new JLabel("Dodaj s\u0142owo");
+		lblOpis = new JLabel("Dodaj s\u0142owo do grupy");
 		lblOpis.setFont(new Font("Arial", Font.BOLD, 16));
 		lblOpis.setHorizontalAlignment(SwingConstants.CENTER);
-		lblOpis.setBounds(0, 0, 800, 25);
+		lblOpis.setBounds(0, 15, 800, 25);
 		add(lblOpis);
+		
+		tfSlowo = new JTextField();
+		tfSlowo.setFont(new Font("Arial", Font.PLAIN, 16));
+		tfSlowo.setColumns(10);
+		tfSlowo.setBounds(145, 56, 640, 25);
+		add(tfSlowo);
+		
+		lblSlowo = new JLabel("S\u0142owo");
+		lblSlowo.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblSlowo.setFont(new Font("Arial", Font.ITALIC, 16));
+		lblSlowo.setBounds(10, 56, 120, 25);
+		add(lblSlowo);
+		
+		lblTlumaczenie = new JLabel("T\u0142umaczenie");
+		lblTlumaczenie.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblTlumaczenie.setFont(new Font("Arial", Font.ITALIC, 16));
+		lblTlumaczenie.setBounds(10, 97, 120, 25);
+		add(lblTlumaczenie);
+		
+		tfTlumaczenie = new JTextField();
+		tfTlumaczenie.setFont(new Font("Arial", Font.PLAIN, 16));
+		tfTlumaczenie.setColumns(10);
+		tfTlumaczenie.setBounds(145, 97, 640, 25);
+		add(tfTlumaczenie);
+		
+		lblCzescMowy = new JLabel("Cz\u0119\u015B\u0107 mowy");
+		lblCzescMowy.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblCzescMowy.setFont(new Font("Arial", Font.ITALIC, 16));
+		lblCzescMowy.setBounds(10, 138, 120, 25);
+		add(lblCzescMowy);
+		
+		tfCzescMowy = new JTextField();
+		tfCzescMowy.setFont(new Font("Arial", Font.PLAIN, 16));
+		tfCzescMowy.setColumns(10);
+		tfCzescMowy.setBounds(145, 138, 640, 25);
+		add(tfCzescMowy);
+		
+		btnPotwierdzAkcje = new JButton("Dodaj s\u0142owo");
+		btnPotwierdzAkcje.setFont(new Font("Arial", Font.PLAIN, 16));
+		btnPotwierdzAkcje.setBounds(635, 179, 150, 30);
+		add(btnPotwierdzAkcje);
 	}
 	
 	/**
 	 * Modyfikuje komponenty na potrzeby edycji s³owa.
 	 */
 	private void modyfikujKomponenty() {
-		lblOpis.setText("Modyfikuj s³owo o ID: " + id_slowo);
+		//lblOpis.setText("Modyfikuj s³owo o ID: " + id_slowo);
 	}
 }
